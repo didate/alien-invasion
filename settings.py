10003ef6ca74e6cab1b30dd2c776c55ec1221e36
didate <lyve.diallo@gmail.com>
2020-02-08 12:33:05 +0000

    init pygame, add screen setting file, add ship in the screen

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        # Screen settings
+        self.screen_width = 900
+        self.screen_hight = 500
+        self.bg_color = (230, 230, 230)
