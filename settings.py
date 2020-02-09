a1155350229cba8a72264e18357e7eb5a16c8319
didate <lyve.diallo@gmail.com>
2020-02-09 17:18:19 +0000

    left and right move of ship

--- settings.py
+++ settings.py
@@ -6,3 +6,6 @@ class Settings():
         self.screen_width = 900
         self.screen_hight = 500
         self.bg_color = (230, 230, 230)
+
+        # Ship speed
+        self.ship_speed_factor = 1.5
